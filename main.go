0b4e949fe1422b5f1fb186d8e83cb5d2465c62b1
TAKUI MASUI <kiiroitori1990.02@gmail.com>
2022-01-08 14:36:02 +0000

    メイン処理実装

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/ChimeraCoder/anaconda"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type TwitterSetting struct {
+	ConsumerApiKey    string `yaml:"CONSUMER_API_KEY"`
+	ConsumerSecretKey string `yaml:"CONSUMER_SECRET_KEY"`
+	AccessApiKey      string `yaml:"ACCESS_API_KEY"`
+	AccessSecretKey   string `yaml:"ACCESS_SECRET_KEY"`
+}
+
+func main() {
+	//　APIツールの設定
+	twitter := TwitterSetting{}
+	conf, _ := os.ReadFile("config.yaml")
+	yaml.Unmarshal(conf, &twitter)
+	anaconda.SetConsumerKey(twitter.ConsumerApiKey)
+	anaconda.SetConsumerSecret(twitter.ConsumerSecretKey)
+
+	client := anaconda.NewTwitterApi(twitter.AccessApiKey, twitter.AccessSecretKey)
+	v := url.Values{}
+	v.Set("count", "30")
+	next_cursor := "-1"
+
+	// 全フォロワーサーチ
+	for {
+		v.Set("cursor", next_cursor)
+		c, err := client.GetFollowersList(v)
+
+		fmt.Println(err)
+
+		for _, usr := range c.Users {
+			fmt.Println(usr.Name)
+		}
+
+		next_cursor = c.Next_cursor_str
+		if err != nil || next_cursor == "0" {
+			break
+		}
+	}
+}
